12e88931abc3f05670715d602e186a13f16fd219
nhyoungboy <118877332+nhyoungboy@users.noreply.github.com>
2025-01-01 08:33:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,6 +10,25 @@
 # See /LICENSE for more information.
 #
 
-# Uncomment a feed source
-sed -i '$a src-git smpackage https://github.com/gxnas/OpenWrt_Build_x64_Packages' feeds.conf.default
-# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+# 1-添加 PowerOff 插件
+git clone https://github.com/nhyoungboy/luci-app-poweroff.git package/luci-app-poweroff
+
+# 2-添加 Mosdns 插件
+rm -rf feeds/packages/lang/golang
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/luci/applications/luci-app-mosdns
+# git clone https://github.com/sbwml/luci-app-mosdns.git package/lean/luci-app-mosdns
+
+# 3-添加 Openclash 插件
+wget -O package/openclash.zip https://codeload.github.com/vernesong/OpenClash/zip/refs/heads/master
+unzip -d package/openclash package/openclash.zip
+cp -r package/openclash/OpenClash-master/luci-app-openclash package/lean/luci-app-openclash
+rm -rf package/openclash package/openclash.zip
+
+# 4-添加 onliner 插件
+sed -i '$a src-git onliner https://github.com/nhyoungboy/luci-app-onliner' feeds.conf.default
+
+# 5-添加  主题
+# rm -rf feeds/luci/themes/luci-theme-argon
+# git clone https://github.com/sbwml/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+git clone --depth=1 https://github.com/nhyoungboy/luci-theme-opentomcat.git package/luci-theme-opentomcat
